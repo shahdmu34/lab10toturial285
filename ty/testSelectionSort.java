5f22070b1fecfcb384c1b05eb91c6361890972c9
Shahd Mustafa <shahdmu@umich.edu>
2022-04-26 11:27:36 -0400

    commit mix and duplicates

--- ty/testSelectionSort.java
+++ ty/testSelectionSort.java
@@ -13,44 +13,6 @@ testDuplicates();
     public testSelectionSort() {
     }
     
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 6;
-        arr[3] = 1;
-        arr[4] = 10;
-        
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 1;
-        Sortedarr[1] = 6;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
-        int[] result = new SelectionSort().basicSelectionSort(arr);
-        assertArrayEquals(Sortedarr, result);
-        /** add tests to check for this unit test **/
-    }
-    
-    public void testNegative(){
-    	 int[] arr = new int[5];
-         arr[0] = -8;
-         arr[1] = -9;
-         arr[2] = -6;
-         arr[3] = -1;
-         arr[4] = -10;
-         
-         int[] Sortedarr = new int[5];
-         Sortedarr[0] = -10;
-         Sortedarr[1] = -9;
-         Sortedarr[2] = -8;
-         Sortedarr[3] = -6;
-         Sortedarr[4] = -1;
-         int[] result = new SelectionSort().basicSelectionSort(arr);
-         assertArrayEquals(Sortedarr, result);
-        /** Test data contains negative values only **/
-    }
-    
     public void testMixed(){
     	 int[] arr = new int[5];
          arr[0] = -9;
@@ -58,7 +20,7 @@ testDuplicates();
          arr[2] = 2;
          arr[3] = 8;
          arr[4] = 10;
-         
+
          int[] Sortedarr = new int[5];
          Sortedarr[0] = -9;
          Sortedarr[1] = -7;
@@ -69,7 +31,7 @@ testDuplicates();
          assertArrayEquals(Sortedarr, result);
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
     	 int[] arr = new int[5];
          arr[0] = 2;
@@ -77,7 +39,7 @@ testDuplicates();
          arr[2] = 6;
          arr[3] = 8;
          arr[4] = 10;
-         
+
          int[] Sortedarr = new int[5];
          Sortedarr[0] = 2;
          Sortedarr[1] = 6;
